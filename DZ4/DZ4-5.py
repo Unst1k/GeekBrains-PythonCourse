dbe831d0a46f77708f912f5a1bd0000b9ac8b3a6
Unst1k <unst1k@mail.ru>
2020-11-21 17:34:21 +0300

    minor issues

--- DZ4/DZ4-5.py
+++ DZ4/DZ4-5.py
@@ -5,11 +5,11 @@ from functools import reduce
 
 
 def total_number(current_number, next_number):
-    return current_number + next_number
+    return current_number * next_number
 
 
 numbers = [i for i in range(100, 1001) if i % 2 == 0]
 
 total = reduce(total_number, numbers)
 
-print(f'\nСумма всех четных чисел от 100 до 1000 - {total}.')
+print(f'\nПроизведение всех четных чисел от 100 до 1000 - {total}.')
